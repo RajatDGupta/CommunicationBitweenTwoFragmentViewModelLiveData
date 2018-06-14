a104a4f494290edfa5e664eda0a372c4fe1db5a4
Naresh Define <ngupta.gupta53@gmail.com>
2018-06-14 17:54:10 +0530

    tenth change

--- app/src/main/java/com/example/viewmodel_livedata/MainActivity.java
+++ app/src/main/java/com/example/viewmodel_livedata/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hi neeraj Gupta git hi sdfa sd
+        // hi neeraj Gupta git hi s dfasd
     }
 }
