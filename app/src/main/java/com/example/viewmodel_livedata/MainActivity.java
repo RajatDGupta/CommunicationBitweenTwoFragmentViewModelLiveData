05f3d362c2780a2441c10439fa28b8eeff352af4
Naresh Define <naresh.battulwar@definelabs.com>
2018-06-14 17:14:47 +0530

    sixth change

--- app/src/main/java/com/example/viewmodel_livedata/MainActivity.java
+++ app/src/main/java/com/example/viewmodel_livedata/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hi neeraj Gupta whatsapp how are you
+        // hi neeraj Gupta
     }
 }
